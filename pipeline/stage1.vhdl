8d4fcec17723331b59dfb6a0551eb3a29ab2cf44
Diego Alberto Barriga Martinez <umoqnier@gmail.com>
2018-06-05 12:13:13 -0500

    Correcciones al stage 1

--- pipeline/stage1.vhdl
+++ pipeline/stage1.vhdl
@@ -4,58 +4,40 @@ library ieee;
 
 entity stage1 is
   port(
-      clk, branch, reset, load : in std_logic;
+      clk, branch, reset : in std_logic;
       dato_w : in unsigned(15 downto 0);
-      PC : out unsigned(15 downto 0);
+      pc : out unsigned(15 downto 0);
       instr : out unsigned(31 downto 0)
   );
 end stage1;
 
 architecture arch of stage1 is
  signal out_mux : unsigned(15 downto 0);
- signal out_PC : unsigned(15 downto 0);
+ signal out_pc : unsigned(15 downto 0);
  signal out_incr : unsigned(15 downto 0);
 
  begin
    -- Mux de branch
-   process(branch, dato_w, out_incr)
-    begin
-      if branch = '0' then
-        out_mux <= dato_w;
-      else
-        out_mux <= out_incr;
-      end if;
-    end process;
+    out_mux <= out_incr when branch = '0' else dato_w;
 
    -- Contador de programa
-   process(clk, reset, load, out_mux)
+   process(clk, reset)
     begin
       if reset = '1' then
-        out_PC <= "0000000000000000";
+        out_pc <= "0000000000000000";
       else
         if rising_edge(clk) then
-          if load = '1' then
-            out_PC <= out_mux;
-          else
-            out_PC <= out_PC;
-          end if;
+          out_pc <= out_mux;
         end if;
       end if;
    end process;
 
    -- Memoria de instrucciones
-
+   -- TODO: Añadir memoria e instanciar
+   
    -- Incrementador
-   process(clk, out_PC)
-    begin
-      if rising_edge(clk) then
-        out_incr <= out_PC + 1;
-      end if;
-    end process;
+    out_incr <= out_pc + 1;
 
-    process(out_incr)
-     begin
-       PC <= out_incr;
-    end process;
+    pc <= out_incr;
 
 end architecture;
